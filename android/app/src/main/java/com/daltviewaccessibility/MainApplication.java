05f2793143133f18dcc88ef23b29f3f5631f2c2b
eliveltonsf <elivelton.seven@gmail.com.br>
2022-01-30 02:50:58 -0300

    add canvas

--- android/app/src/main/java/com/daltviewaccessibility/MainApplication.java
+++ android/app/src/main/java/com/daltviewaccessibility/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.bvic23.rngetpixel.RNPixelColorPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
--- src/pages/Imageview/index.js
+++ src/pages/Imageview/index.js
@@ -2,24 +2,34 @@ import React, { useEffect } from 'react';
 
 import {Container, ProductAreaView, Image} from './styles';
 
-import PixelColor from 'react-native-pixel-color';
+import Canvas, {Image as CanvasImage} from 'react-native-canvas';
 
 export default function Imageview({route}) {
 
-  useEffect(() => {
-    PixelColor.getHex(route.params.paramKey, {x: 50, y: 60}).then().catch((err) => {
-       console.log(err)
-     });
-  }, []);  
+  handleCanvas =  (canvas) => {
+    const image = new CanvasImage(canvas);
+    canvas.width = 100;
+    canvas.height = 100;
+    
+    const context = canvas.getContext('2d');
+
+    image.src = route.params.paramKey;
+
+    //'https://image.freepik.com/free-vector/unicorn-background-design_1324-79.jpg'; Note: with this uri everything works well
+
+     image.addEventListener('load', () => {
+      debugger
+      console.log('image is loaded');
+      context.drawImage(image, 0, 0, 100, 100);
+    }); 
+  }
+
 
 
   return (
     <Container >
       <ProductAreaView>
-        <Image
-          source={{uri: route.params.paramKey}}
-        />
-
+        <Canvas ref={this.handleCanvas}/>
       </ProductAreaView>
     </Container>
   );
